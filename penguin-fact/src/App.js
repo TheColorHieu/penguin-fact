ed4c445a6971d783bd7f48024b6cd6a231b5d438
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-08-26 21:32:51 -0700

    added more facts

--- penguin-fact/src/App.js
+++ penguin-fact/src/App.js
@@ -11,8 +11,13 @@ function App() {
     "Penguins are carnivores.",
     "Penguins have black and white feathers that look like a tuxedo.",
     "Penguins live in the Southern Hemisphere.",
-    "Penguins live in the Southern Hemisphere.",
-    "Penguins live in the Southern Hemisphere.",
+    "Penguins waddle from side to side when they walk.",
+    "Penguins live in large colonies called rookeries.",
+    "Penguins are social birds.",
+    "Penguins have waterproof feathers.",
+    "Penguins eat a range of fish and other sealife that they catch underwater.",
+    "Penguins can drink sea water.",
+    "Penguins can't fly.",
   ];
 
   const generateRandomFact = async () => {
